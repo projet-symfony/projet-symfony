119545d3ef8e1eb094c750038378b059f2a12058
Stili Mehdi <mehdidi93420@gmail.com>
2019-03-11 00:00:30 +0100

    First commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 11/02/2019
+ * Time: 10:36
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage(){
+        return $this->render('home.html.twig');
+    }
+
+    /**
+     * @Route("/article/{slug}")
+     */
+    public function readAll($slug){
+
+        var_dump($slug);
+        return new Response('For All Article');
+    }
+
+
+    public function read($slug){
+        return $this->render('article/read.html.twig');
+    }
+}
