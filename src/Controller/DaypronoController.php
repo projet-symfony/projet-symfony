5df6025a320679cf6eba0d7a3c5736214f1c40ed
linaamayro <linaamayri@gmail.com>
2019-03-25 12:01:24 +0100

    db

--- src/Controller/DaypronoController.php
+++ src/Controller/DaypronoController.php
@@ -8,16 +8,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Match;
+use App\Entity\Vote;
+use App\Repository\VoteRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DaypronoController extends AbstractController
 {
+    private $repository ;
+    private $em;
+    public function __construct(VoteRepository $repository, ObjectManager $em)
+{
+   $this->repository = $repository;
+   $this->em = $em;
+}
+
     /**
      * @Route("/dayprono")
      */
     public function homepage(){
-        return $this->render('dayprono.html.twig');
+        $vote = new Vote();
+        $vote->setNbVote(3)
+            ->setIdMatch(2)
+            ->setVote1(100)
+            ->setVote2(100)
+            ->setVoteN(15);
+        $this->em->persist($vote);
+        $this->em->flush();
+        $votes = $this->repository->printAll();
+            return $this->render('dayprono.html.twig', [
+                'vote' => $votes]);
+        //$vote=$this->getDoctrine()->getRepository(Vote::class)->findAll();
+        //$match=$this->getDoctrine()->getRepository(Match::class)->findAll();
+        //return $this->render('dayprono.html.twig',array('vote'=>$vote,'match'=>$match));
     }
 }
