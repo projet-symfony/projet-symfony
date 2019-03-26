569dba92c3731b95a34c82b090564551b89f1f48
MariamHmaida <hmaida.mariama@gmail.com>
2019-03-26 11:25:29 +0100

    new entities

---
+++ src/Entity/Equipe.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EquipeRepository")
+ */
+class Equipe
+{
+    /**
+     * @ORM\Igit addd()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $NomEquipe;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Pays;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $nbMatchJoue;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $nbMatchGagne;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $nbMatchPerdu;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $nbPronostique;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomEquipe(): ?string
+    {
+        return $this->NomEquipe;
+    }
+
+    public function setNomEquipe(string $NomEquipe): self
+    {
+        $this->NomEquipe = $NomEquipe;
+
+        return $this;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->Pays;
+    }
+
+    public function setPays(string $Pays): self
+    {
+        $this->Pays = $Pays;
+
+        return $this;
+    }
+
+    public function getNbMatchJoue(): ?int
+    {
+        return $this->nbMatchJoue;
+    }
+
+    public function setNbMatchJoue(?int $nbMatchJoue): self
+    {
+        $this->nbMatchJoue = $nbMatchJoue;
+
+        return $this;
+    }
+
+    public function getNbMatchGagne(): ?int
+    {
+        return $this->nbMatchGagne;
+    }
+
+    public function setNbMatchGagne(?int $nbMatchGagne): self
+    {
+        $this->nbMatchGagne = $nbMatchGagne;
+
+        return $this;
+    }
+
+    public function getNbMatchPerdu(): ?int
+    {
+        return $this->nbMatchPerdu;
+    }
+
+    public function setNbMatchPerdu(?int $nbMatchPerdu): self
+    {
+        $this->nbMatchPerdu = $nbMatchPerdu;
+
+        return $this;
+    }
+
+    public function getNbPronostique(): ?int
+    {
+        return $this->nbPronostique;
+    }
+
+    public function setNbPronostique(?int $nbPronostique): self
+    {
+        $this->nbPronostique = $nbPronostique;
+
+        return $this;
+    }
+}
