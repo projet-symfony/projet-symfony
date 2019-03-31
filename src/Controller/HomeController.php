023d23a34a7c4bfff315eba85dfc609ba292161f
MhdStill <stili.mehdi@yahoo.com>
2019-03-31 21:13:10 +0200

    connexion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,8 +7,12 @@
  */
 
 namespace App\Controller;
+use App\Entity\Utilisateur;
+use App\Form\UserForm;
+use App\Repository\UtilisateurRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -16,22 +20,28 @@ class HomeController extends AbstractController
     /**
      * @Route("/")
      */
-    public function homepage(){
-        return $this->render('home.html.twig');
+    public function homepage(ObjectManager $om, Request $request, UtilisateurRepository $repository){
+        $user = new Utilisateur();
+        $form   = $this->createForm(UserForm::class, $user);
+        $form->handleRequest($request);
+
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository(Utilisateur::class)
+        ;
+
+        $listUser = $repository->findAll();
+        foreach($listUser as $us){
+            if ($form->get('Pseudo')->getData()==$us->getLogin() && $form->get('Password')->getData()==$us->getPassword()){
+                return $this->render('homeConnected.html.twig');
+             }
+
+        }
+
+        return $this->render('home.html.twig',array('form'=>$form));
+
     }
 
-    /**
-     * @Route("/article/{slug}")
-     */
-    public function readAll($slug){
-
-        var_dump($slug);
-        return new Response('For All Article');
-    }
-
-
-    public function read($slug){
-        return $this->render('article/read.html.twig');
-    }
 
 }
